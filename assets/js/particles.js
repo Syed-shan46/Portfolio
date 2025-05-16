5b9c9b8f69c398c81fa21e23cf597a1de7fdf16b
Syed S <syedshan093@gmail.com>
2025-05-16 16:36:47 +0530

    added particles

---
+++ assets/js/particles.js
@@ -0,0 +1,34 @@
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: 80,
+      density: { enable: true, value_area: 800 }
+    },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: {
+      value: 0.5,
+      random: true
+    },
+    size: {
+      value: 2,
+      random: true
+    },
+    move: {
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: false },
+      onclick: { enable: false }
+    }
+  },
+  retina_detect: true
+});
